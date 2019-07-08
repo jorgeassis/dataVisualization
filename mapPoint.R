bfd210c0a23f6c1212339685e15619ba372e48d1
Muffins and Code <jorgemfa@gmail.com>
2019-07-08 15:28:19 +0100

    Up.

--- mapPoint.R
+++ mapPoint.R
@@ -17,7 +17,7 @@ source("mainFunctions.R")
 ## --------------------------------------------------
 ## Custom Files, Extent and Colors
 
-cExtent <- extent(c(10,30,-38,-15))
+cExtent <- extent(c(-180,180,-90,-90))
 cExtent <- getMapExtent()
 
 cColors <- c("#8EB0D6", "#F8F4C1","#E29939","#AA1313")
@@ -28,14 +28,26 @@ oceanColor <- "#ffffff"
 ## ----------------------------------------------------------------------------------------------------
 ## Point data Map
 
+source("https://raw.githubusercontent.com/jorgeassis/marineforestsDB/master/sourceMe.R")
+dataset <- extractDataset("brownAlgae",pruned=TRUE)
+dataset <- dataset[dataset$taxOrder %in% c("Laminariales","Tilopteridales"),]
+pointData <- dataset[,c("decimalLongitude","decimalLatitude")] 
+colnames(pointData) <- c("Lon","Lat")
+
 pointData <- "/Volumes/Jellyfish/Dropbox/Manuscripts/The Modelling Factory/Kelp/1. Laminaria pallida (Surface)/Data/Occurrences/Laminaria pallida Processed.csv"
 pointData <- read.csv(pointData,sep=";",header=T)[,c("Lon","Lat")]
 pointData <- pointData[pointData$Lon >= 14,]
 
-titleMap <-  "" 
-subtitleMap <-  ""
-captionMap <- "Distribution records used for modelling"
-fillLabel <- "Probability"
+## -----------------
+
+pointData <- pointData[!duplicated(pointData),]
+
+## -----------------
+
+titleMap <-  "Distribution of kelp forest records worldwide" 
+subtitleMap <-  "Taxonomic orders Laminariales and Tilopteridales"
+captionMap <- ""
+fillLabel <- ""
 
 elementsToHide <- c("legend","graticulate") # legend graticulate
 addLabelsPlot()
@@ -46,12 +58,12 @@ worldMap <- ne_countries(scale = 10, returnclass = "sp")
 worldMap <- crop(worldMap,cExtent)
 worldMap <- aggregate(worldMap,dissolve=T)
 
-buffer <- c(1,1,1,1)
+buffer <- c(0,0,0,0)
 
 Map1 <- ggplot() +
         geom_polygon(data = worldMap, aes(x = long, y = lat, group = group), fill="#ded9cd", colour = NA) +
         geom_path(data = worldMap, aes(x = long, y = lat, group = group), color = "#767676", size = 0.1) +
-        geom_point(data = pointData, aes(x = Lon, y = Lat), color = "#565656") + 
+        geom_point(data = pointData, aes(x = Lon, y = Lat), color = "#9A3B04") + 
         coord_equal() +
         theme_map() + labelsPlot
 
