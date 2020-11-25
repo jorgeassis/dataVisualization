60e737bca748a86e94cc180cb864ba26c6a8f02f
Jorge Assis <jorgeassis@jorges-mac-pro.ualg.pt>
2020-11-25 22:47:07 +0000

    Up.

--- mapRasterH3Polygons.R
+++ mapRasterH3Polygons.R
@@ -41,15 +41,17 @@ theme_map <-
 ## Custom Files, Extent and Colors
 
 mainDirectory <- "../Results/_ Backup/_ Ensembles/"
+
+## ------------------------
+# Europe projection
+
 worldMap <- ne_countries(scale = 10, returnclass = "sf")
 
-## ------------
-
 mapExtent = c(xmin = -35, ymin = 20, xmax = 50.05, ymax = 85)
-
-worldMap <- st_crop(worldMap,mapExtent)
 worldMap <- worldMap[worldMap$continent != "North America",]
 worldMapCoordRef <- paste0("+proj=laea +lat_0=",mapExtent[4]-mapExtent[3]," +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs")
+
+worldMap <- st_crop(worldMap,mapExtent)
 
 mainGlobalMap <- ggplot() + 
   geom_sf(data = worldMap,fill="#ABABAB", colour = "#F3F3F3" , size=0.1 ) +
@@ -57,9 +59,34 @@ mainGlobalMap <- ggplot() +
   theme_map + coord_sf(crs = worldMapCoordRef)
 mainGlobalMap
 
+## ------------------------
+# N Pole projection
+
+worldMap <- ne_countries(scale = 10, returnclass = "sp")
+mapExtent = c(xmin = -180, ymin = 45, xmax = 180, ymax = 90)
+worldMap <- crop(worldMap,extent(mapExtent["xmin"],mapExtent["xmax"],mapExtent["ymin"],mapExtent["ymax"]))
+
+x_lines <- seq(-120,180, by = 60)
+
+mainGlobalMap <- ggplot() + 
+  geom_polygon(data = worldMap,aes(x = long, y = lat, group = group),fill="#ABABAB", colour = "#F3F3F3" , size=0.1 ) +
+  theme(axis.ticks=element_blank()) + ylab("") + xlab("") +
+  theme_map + coord_map("ortho", orientation = c(90, 0, 0)) +
+  theme(legend.position = "none") +
+  theme(text = element_text(family = "Helvetica", color = "#22211d")) +
+  theme(plot.background = element_rect(fill = "#FFFFFF", color = NA),panel.background = element_rect(fill = "#FFFFFF", color = NA), axis.ticks=element_blank()) +
+  scale_y_continuous(breaks = seq(45, 90, by = 10), labels = NULL) +
+  scale_x_continuous(breaks = NULL) + xlab("") +  ylab("") +
+  geom_hline(aes(yintercept = 45), size = 0.5, colour = "gray")  +
+  geom_segment(size = 0.1,aes(y = 45, yend = 90, x = x_lines, xend = x_lines), linetype = "dashed")
+  
+mainGlobalMap
+
+## ------------
+
 rasters <- list.files(mainDirectory,full.names = TRUE,pattern="tif")
 rasters
-file <- 31 # 31
+file <- 1 # 31
 rasterMap <- raster(rasters[file])
 names(rasterMap)
 # rasterMap[rasterMap == 0.2] <- NA
@@ -73,7 +100,10 @@ rasterMapDF <- data.frame(xyFromCell(rasterMap, Which( !is.na(rasterMap) , cells
 rasterMapDF <- data.frame(rasterMapDF,hex=apply(rasterMapDF[,1:2],1,function(x) { getIndexFromCoords(x[[2]], x[[1]], resolution = resolutionH3) } ))
 rasterMapDF <- data.frame(hex=unique(rasterMapDF$hex),val=sapply(unique(rasterMapDF$hex),function(x) { mean(rasterMapDF[rasterMapDF$hex == x , "val"]) } ))
 rasterMapDF.polygons <- h3_to_geo_boundary_sf(rasterMapDF$hex)
-rasterMapDF.polygons$Value <- rasterMapDF$val
+rasterMapDF.polygons$hex <- rasterMapDF$hex
+rasterMapDF.polygons$value <- rasterMapDF$val
+
+max(rasterMapDF$val)
 
 ## ----------------------------------------------------------------------------------------------------
 ## ----------------------------------------------------------------------------------------------------
@@ -82,10 +112,10 @@ fieldLabel <- "Species richness\n[number]"
 fieldLabel <- "Species gain\n[number]"
 
 ## -----------------
-# plasma inferno magma viridis
+## As Sf
 
-plot2 <- mainGlobalMap +
-         geom_sf(data = rasterMapDF.polygons, aes(fill = Value) , size=0.1) +
+plot1 <- mainGlobalMap +
+         geom_sf(data = rasterMapDF.polygons, aes(fill = value) , size=0.1) +
          scale_fill_viridis_c(option = 'magma', direction = -1, begin = 0, end = 1) +
          coord_sf(crs = worldMapCoordRef) + labs(fill = fieldLabel) +
          theme(legend.position=c(.95, .95),
@@ -93,8 +123,33 @@ plot2 <- mainGlobalMap +
                legend.margin=margin(0,0,0,0),
                legend.box.margin=margin(0,0,0,-80))
 
-plot2
+plot1
+
+## -----------------
+# As Sp 
+
+rasterMapDF.polygons <- sf:::as_Spatial(rasterMapDF.polygons)
+rasterMapDF.polygons$value <- as.numeric(as.character(rasterMapDF.polygons$value))
+rasterMapDF.polygons$hex <- as.character(rasterMapDF.polygons$hex)
+
+rasterMapDF.polygons <- fortify(rasterMapDF.polygons)
+rasterMapDF.polygons <- rasterMapDF.polygons[rasterMapDF.polygons$lat >= extent(worldMap)[3],]
+rasterMapDF.polygons$value <- sapply(as.numeric(rasterMapDF.polygons$id), function(x) { rasterMapDF[x,"val"] })
+
+plot1 <- mainGlobalMap +
+  geom_polygon(data = rasterMapDF.polygons, aes(x = long, y = lat,group=id, fill=value), colour = NA, size = 0.1) +
+  scale_fill_viridis_c(name= fieldLabel, option = 'magma', direction = -1, begin = 0, end = 1, limits=c(0,100)) +
+  theme(legend.position=c(.99, .99),
+        legend.justification=c("right", "top"),
+        legend.margin=margin(0,0,0,0),
+        legend.box.margin=margin(0,0,0,-80),legend.title=element_blank(),legend.background = element_rect(fill = "#ffffff", color = NA))
+
+plot1
+
 # + theme(legend.position = "none")
+
+## -----------------
+## -----------------
 
 pdf(file=paste0(mainDirectory,"/"),width=12,height=12,useDingbats=FALSE)
 plot1
