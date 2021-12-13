47b786033b82933a6f5a835219f0c13d376719a1
theMarineDataScientist <jorgemfa@gmail.com>
2021-12-13 22:10:27 +0000

    Up.

--- Projects/seaSurfaceOxygen/mainScript.R
+++ Projects/seaSurfaceOxygen/mainScript.R
@@ -5,6 +5,7 @@ library(raster)
 library(rnaturalearth)
 library(sf)
 library(geosphere)
+library(rgeos)
 
 # Setting the working directory
 setwd("Projects/seaSurfaceOxygen")
@@ -21,24 +22,21 @@ bb <- sf::st_union(sf::st_make_grid(
 bb <- st_transform(bb, projection)
 
 # Read oxygen layer
-oxygenLayer <- raster("Data/DissolvedMolecularOxygen Surface Pred Mean.tif")
-
-oxygenLayer <- aggregate(oxygenLayer, 12)
-
+oxygenLayer <- raster("Data/DissolvedMolecularOxygen.tif")
 oxygenLayer <- projectRaster(oxygenLayer, crs = projection)
 oxygenLayer <- mask(oxygenLayer, as(bb, "Spatial"))
 oxygenLayer <- as.data.frame(oxygenLayer,xy=TRUE,na.rm=T)
 colnames(oxygenLayer) <- c("Lon","Lat","Val")
 
 # Read the shapefile depicting global landmasses
-worldMap <- shapefile("Data/LandMass Polygon LR.shp")
+worldMap <- shapefile("Data/LandMass Polygon.shp")
 worldMap <- spTransform(worldMap, CRSobj = projection)
+worldMap <- gBuffer(worldMap, byid=TRUE, width=0.001)
 worldMap <- crop(worldMap, as(bb, "Spatial"))
 
 ## -----------------
 
 theme_map <- 
-  theme_minimal() +
   theme(
     text = element_text(family = "Helvetica", color = "#22211d"),
     axis.text.x = element_blank(),
@@ -48,12 +46,14 @@ theme_map <-
     axis.title.y = element_blank(),
     panel.grid.major = element_line(color = "black", size = 0.1),
     panel.grid.minor = element_blank(),
-    plot.background = element_rect(fill = "#FFFFFF", color = NA), # F3F3F3
-    panel.background = element_rect(fill = "#FFFFFF", color = NA), # F3F3F3
-    legend.background = element_rect(fill = "#FFFFFF", color = NA),
+    plot.background = element_rect(fill = "#FFFFFF", color = NA),
+    panel.background = element_rect(fill = "#FFFFFF", color = NA),
     panel.border = element_blank(),
+    legend.background = element_rect(fill = "#FFFFFF", color = NA),
     legend.position="bottom", 
-    legend.margin=margin(-5,0,0,0),
+    legend.box = "horizontal",
+    legend.margin=margin(0,0,0,0),
+    legend.box.margin=margin(-10,-10,-10,-10),
     legend.key.height= unit(0.25, 'cm'),
     legend.key.width= unit(0.75, 'cm') )
 
@@ -63,12 +63,10 @@ theme_map <-
 myColors <- c("#6FBBE8","#A1ECD8","#F6F9AB","#FCB46D","#B21414")
 myColors <- c("#6FBBE8","#A1ECD8","#F6F9AB","#FCB46D","#B21414","#D278E4","#9914B3")
 
-# A1A1A1
-
 plot <- ggplot() +
-  geom_polygon(data = worldMap, aes(x = long, y = lat, group = group), fill="#FFFFFF", colour = "#FFFFFF" , size=0.25 ) +
   geom_tile(data = oxygenLayer, aes(x=Lon,y=Lat,fill=Val)) +
-  scale_fill_gradientn(guide_legend(title="Sea surface dissolved oxygen (mmol.m3)"), colours=myColors, na.value='transparent') +
+  scale_fill_gradientn(guide = guide_legend(title="Sea surface dissolved oxygen [mmol.m3]", direction = "horizontal", title.position = "top", title.hjust = 0.5), colours=myColors, na.value='transparent') +
+  geom_polygon(data = worldMap, aes(x = long, y = lat, group = group), fill="#A1A1A1", colour = "#A1A1A1" , size=0.25 ) +
   geom_sf(data = bb,fill=NA, colour = "white" , linetype='solid', size=2 ) +
   theme_map
 plot
